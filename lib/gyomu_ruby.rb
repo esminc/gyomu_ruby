1d084e71c530d4458c012f82ce7ede462b1ad806
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-02-09 08:48:40 +0900

    oops

--- lib/gyomu_ruby.rb
+++ lib/gyomu_ruby.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
 require 'gyomu_ruby/core_ext/time'
+require 'gyomu_ruby/core_ext/numeric'
 require 'gyomu_ruby/amazon_web_service/file_bucket'
 require 'gyomu_ruby/ordinary_system_development'
