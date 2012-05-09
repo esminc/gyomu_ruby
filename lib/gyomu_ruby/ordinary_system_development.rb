19c9681dcc07c1d2bb37c68a48978513169f968d
moro <moronatural@gmail.com>
2012-05-09 16:53:01 +0900

    add autoload family

--- lib/gyomu_ruby/ordinary_system_development.rb
+++ lib/gyomu_ruby/ordinary_system_development.rb
@@ -6,6 +6,7 @@ module GyomuRuby
     autoload 'AllYourSiteAreBelongToSsl', osd_dir + 'all_your_site_are_belong_to_ssl'
     autoload 'Translation', osd_dir + 'translated_from'
     autoload 'WordNormalizer', osd_dir + 'word_normalizer'
+    autoload 'AddressFormatter', osd_dir + 'address_formatter'
   end
   OSD = OrdinarySystemDevelopment
 end
