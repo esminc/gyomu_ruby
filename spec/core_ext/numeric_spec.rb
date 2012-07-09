fd0165f9b3fd29a2a11106817a4ccd2b3548e4dd
moro <moronatural@gmail.com>
2012-07-09 19:33:13 +0900

    fix broken tests

--- spec/core_ext/numeric_spec.rb
+++ spec/core_ext/numeric_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 require 'gyomu_ruby/core_ext/numeric'
 
 describe Numeric, 'gyomu_ruby extentions' do
-  describe '#roundup' do
-    it { 101.roundup(50).should == 150 }
-    it { 151.roundup(50).should == 200 }
-    it { 123.roundup(3).should == 123 }
-    it { 10.1.roundup(2).should == 12 }
+  describe '#round_up' do
+    it { 101.round_up(50).should == 150 }
+    it { 151.round_up(50).should == 200 }
+    it { 123.round_up(3).should == 123 }
+    it { 10.1.round_up(2).should == 12 }
   end
 
-  describe '#rounddown' do
-    it { 101.rounddown(50).should == 100 }
-    it { 151.rounddown(50).should == 150 }
-    it { 123.rounddown(3).should == 123 }
-    it { 10.1.rounddown(2).should == 10 }
+  describe '#round_down' do
+    it { 101.round_down(50).should == 100 }
+    it { 151.round_down(50).should == 150 }
+    it { 123.round_down(3).should == 123 }
+    it { 10.1.round_down(2).should == 10 }
   end
 end
