ab0b61bd6ed90884b7ccff221b389495d5485498
moro <moronatural@gmail.com>
2012-08-20 18:27:46 +0900

    revert const initialization from 1fbd38dddeee1ae35

--- lib/gyomu_ruby/address_formatter.rb
+++ lib/gyomu_ruby/address_formatter.rb
@@ -4,6 +4,16 @@ require 'moji'
 
 module GyomuRuby
   module AddressFormatter
+    PREFS = File.read(
+      File.expand_path('../../masters/prefectures.csv', File.dirname(__FILE__))
+    ).lines.map(&:strip).freeze
+
+    ext_tel_numbers = File.read(
+      File.expand_path('../../masters/ext_tel_numbers.csv', File.dirname(__FILE__))
+    ).lines.map{|n| "0#{n.strip}" }
+
+    EXT_TEL_NUMBERS = (ext_tel_numbers + %w[050 070 080 090 0120]).sort.reverse.freeze
+    EXT_TEL_NUMBERS_RE = /\A(#{EXT_TEL_NUMBERS.sort.reverse.join('|')})/uo
 
     module_function
 
