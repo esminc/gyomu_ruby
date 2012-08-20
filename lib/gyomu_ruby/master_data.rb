22f7aa661751a990b704a6215bc1bc642a4023a1
hibariya <celluloid.key@gmail.com>
2012-08-20 11:59:32 +0900

    GyomuRuby provides one of the popular area assignment method

--- lib/gyomu_ruby/master_data.rb
+++ lib/gyomu_ruby/master_data.rb
@@ -2,31 +2,36 @@
 
 module GyomuRuby
   module MasterData
-    master_data_dir = File.expand_path('../../masters', File.dirname(__FILE__))
+    @@currencies = {
+      'JPY' => '円',
+      'USD' => '米ドル',
+      'EUR' => 'ユーロ'
+    }
 
-    @@currencies_raw_data  = YAML.load_file(master_data_dir + '/currencies.yml')
-    @@prefectures_raw_data = YAML.load_file(master_data_dir + '/prefectures.yml')
+    @@prefectures, @@areas = {}, {}
+    { # https://www.lasdec.or.jp/cms/1,0,14.html
+      '北海道' => %w(北海道),
+      '東北'   => %w(青森県 岩手県 宮城県 秋田県 山形県 福島県),
+      '関東'   => %w(茨城県 栃木県 群馬県 埼玉県 千葉県 東京都 神奈川県),
+      '中部'   => %w(新潟県 富山県 石川県 福井県 山梨県 長野県 岐阜県 静岡県 愛知県),
+      '近畿'   => %w(三重県 滋賀県 京都府 大阪府 兵庫県 奈良県 和歌山県),
+      '中国'   => %w(鳥取県 島根県 岡山県 広島県 山口県),
+      '四国'   => %w(徳島県 香川県 愛媛県 高知県),
+      '九州'   => %w(福岡県 佐賀県 長崎県 熊本県 大分県 宮崎県 鹿児島県 沖縄県)
+    }.each do |area_name, prefecture_names|
+      @@areas[area_name] ||= []
+
+      prefecture_names.each.with_index(@@prefectures.length) do |prefecture_name, index|
+        code = '%02d' % index.next
+
+        @@prefectures[code] = prefecture_name
+        @@areas[area_name] << code
+      end
+    end
 
     class << self
-      def currencies
-        @currencies ||= @@currencies_raw_data.each_with_object({}) {|currency, memo| memo[currency[:code]] = currency[:name] }
-      end
-
-      def prefectures
-        @prefectures ||= @@prefectures_raw_data.each_with_object({}) {|pref, memo| memo[pref[:code]] = pref[:name] }
-      end
-
-      def prefecture_areas
-        @areas ||=
-          @@prefectures_raw_data.group_by {|pref|
-            pref[:area]
-          }.each.with_object({}) {|(area, prefs), memo|
-            memo[area] = prefs.map {|pref| pref[:code] }
-          }
-      end
-
       def currency_name(code)
-        lookup(:currencies, code.upcase)
+        lookup(:currencies, code.to_s.upcase)
       end
 
       def prefecture_name(code)
@@ -34,7 +39,7 @@ module GyomuRuby
       end
 
       def prefecture_codes_by_area(name)
-        lookup(:prefecture_areas, name)
+        lookup(:areas, name)
       end
 
       def lookup(type, val)
@@ -45,10 +50,8 @@ module GyomuRuby
         data(type).invert.to_a # FIXME FormHelper にしたい
       end
 
-      private
-
       def data(type)
-        __send__(type.intern)
+        MasterData.class_variable_get("@@#{type}")
       end
     end
   end
--- spec/gyomu_ruby/master_data_spec.rb
+++ spec/gyomu_ruby/master_data_spec.rb
@@ -1,34 +1,31 @@
 # coding: utf-8
 
 require 'spec_helper'
-require 'gyomu_ruby/master_data'
 
-module GyomuRuby
-  describe MasterData do
-    describe '.prefecture_name' do
-      context 'code in String' do
-        subject { MasterData.prefecture_name('01') }
+describe GyomuRuby::MasterData do
+  describe '.prefecture_name' do
+    context 'code in String' do
+      subject { GyomuRuby::MasterData.prefecture_name('01') }
 
-        it { should == '北海道' }
-      end
-
-      context 'code in Fixnum' do
-        subject { MasterData.prefecture_name(1) }
-
-        it { should == '北海道' }
-      end
+      it { should == '北海道' }
     end
 
-    describe '.prefecture_codes_by_area' do
-      subject { MasterData.prefecture_codes_by_area('関東') }
+    context 'code in Fixnum' do
+      subject { GyomuRuby::MasterData.prefecture_name(1) }
 
-      it { should == %w(08 09 10 11 12 13 14) }
+      it { should == '北海道' }
     end
+  end
 
-    describe '.currency_name' do
-      subject { MasterData.currency_name('USD') }
+  describe '.prefecture_codes_by_area' do
+    subject { GyomuRuby::MasterData.prefecture_codes_by_area('関東') }
 
-      it { should == '米ドル' }
-    end
+    it { should == %w(08 09 10 11 12 13 14) }
+  end
+
+  describe '.currency_name' do
+    subject { GyomuRuby::MasterData.currency_name('USD') }
+
+    it { should == '米ドル' }
   end
 end
