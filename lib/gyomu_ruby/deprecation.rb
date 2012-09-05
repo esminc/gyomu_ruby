f6964960e693dfff4f01002daff7593f46ac2a26
hibariya <celluloid.key@gmail.com>
2012-09-05 12:20:09 +0900

    OrdinarySystemDevelopment (OSD) is deprecated

--- lib/gyomu_ruby/deprecation.rb
+++ lib/gyomu_ruby/deprecation.rb
@@ -1,11 +1,32 @@
 module GyomuRuby
   module Deprecation
     module_function
+
     def deprecated_method_warning(method_name, message)
       warning = "DEPRECATIOPN WARNING:"
       warning = "#{warning} #{method_name} is deprecated and will be removed in the near future"
       warning = "#{warning} (#{message})"
       STDERR.puts(warning)
+    end
+
+    module DeprecatedConstant
+      private
+
+      def self.extended(constant)
+        constant.instance_variable_set :@__new_constants, {}
+      end
+
+      def const_missing(name)
+        return super unless constant = @__new_constants[name]
+
+        STDERR.puts "DEPRECATIOPN WARNING: #{self.name}::#{name} is deprecated and will be removed in the near future(use #{constant.name} instead)"
+
+        constant
+      end
+
+      def deprecated(name, valid_constant)
+        @__new_constants.update name => valid_constant
+      end
     end
   end
 end
