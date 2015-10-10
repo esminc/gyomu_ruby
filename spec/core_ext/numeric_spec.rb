7c97d1bbd4f6bbb2fb58736766889ad7cb7443b4
taki <takiy33@gmail.com>
2015-10-10 22:08:08 +0900

    Convert specs to RSpec 2.99.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 30 conversions
        from: == expected
          to: eq(expected)
    
    * 20 conversions
        from: it { should ... }
          to: it { is_expected.to ... }
    
    * 17 conversions
        from: obj.should
          to: expect(obj).to
    
    * 2 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    * 1 conversion
        from: obj.stub(:message)
          to: allow(obj).to receive(:message)
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/core_ext/numeric_spec.rb
+++ spec/core_ext/numeric_spec.rb
@@ -3,16 +3,16 @@ require 'gyomu_ruby/core_ext/numeric'
 
 describe Numeric, 'gyomu_ruby extentions' do
   describe '#round_up' do
-    it { 101.round_up(50).should == 150 }
-    it { 151.round_up(50).should == 200 }
-    it { 123.round_up(3).should == 123 }
-    it { 10.1.round_up(2).should == 12 }
+    it { expect(101.round_up(50)).to eq(150) }
+    it { expect(151.round_up(50)).to eq(200) }
+    it { expect(123.round_up(3)).to eq(123) }
+    it { expect(10.1.round_up(2)).to eq(12) }
   end
 
   describe '#round_down' do
-    it { 101.round_down(50).should == 100 }
-    it { 151.round_down(50).should == 150 }
-    it { 123.round_down(3).should == 123 }
-    it { 10.1.round_down(2).should == 10 }
+    it { expect(101.round_down(50)).to eq(100) }
+    it { expect(151.round_down(50)).to eq(150) }
+    it { expect(123.round_down(3)).to eq(123) }
+    it { expect(10.1.round_down(2)).to eq(10) }
   end
 end
